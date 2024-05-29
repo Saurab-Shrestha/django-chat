cab59feb52f6a2b59f9d79c487c223a4ce1c0f6f
Saurab-Shrestha <shresthasaurab030@gmail.com>
2024-05-29 10:46:14 +0545

    Added comments in users models.py

--- chat/models.py
+++ chat/models.py
@@ -7,13 +7,30 @@ from channels.layers import get_channel_layer
 User = get_user_model()
 
 class MessageModel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user',
-                             related_name='from_user', db_index=True)
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='recipient',
-                                  related_name='to_user', db_index=True)
+    """
+    Model representing a chat message.
+    """
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        verbose_name='user',
+        related_name='from_user', 
+        db_index=True
+    )
+    recipient = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        verbose_name='recipient',
+        related_name='to_user', 
+        db_index=True
+    )
     
-    timestamp = models.DateTimeField('timestamp', auto_now_add=True, editable=False, 
-                                     db_index=True)
+    timestamp = models.DateTimeField(
+        'timestamp', 
+        auto_now_add=True, 
+        editable=False, 
+        db_index=True
+    )
     body = models.TextField('body')
 
     def __str__(self) -> str:
@@ -21,13 +38,13 @@ class MessageModel(models.Model):
     
     def characters(self):
         """
-        Function to return len of body.
+        Returns the number of characters in the message body.
         """
         return len(self.body)
     
     def notify_ws_clients(self):
         """
-        Inform client there is a new message.
+        Notifies WebSocket clients about the new message.
         """
         notification = {
             'type': 'receive_group_message',
@@ -43,16 +60,18 @@ class MessageModel(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Trims white spaces, saves the message and notifies the recipient via WS if the 
-        message is new.
+        Overrides the save method to trim whitespace and notify WebSocket clients about new messages.
         """
-        new = self.id
+        is_new = self.id
         self.body = self.body.strip()
         super(MessageModel, self).save(*args, **kwargs)
-        if new is None:
+        if is_new is None:
             self.notify_ws_clients()
 
     class Meta:
+        """
+        Meta class for the Message model.
+        """
         app_label = 'chat'
         verbose_name = 'message'
         verbose_name_plural = 'messages'
--- readme.md
+++ readme.md
@@ -1,1 +1,90 @@
 # Real-time chat application with Django
+
+## Overview
+This is a Django rest framework application for real-time chat between users.
+It utilizes Django channels for handling WebSockets and Redis as a message broker.
+
+## Project Structure
+The project is structured as follows:
+
+django-chat/
+* chat
+    * migrations
+    * test
+    * __init__.py
+    * admin.py
+    * apps.py
+    * consumers.py
+    * models.py
+    * routing.py
+    * serializers.py
+    * urls.py
+    * views.py
+* django_chat
+    * __init__.py
+    * asgi.py
+    * routing.py
+    * settings.py
+    * urls.py
+    * wsgi.py
+* users
+    * migrations
+    * test
+    * __init__.py
+    * admin.py
+    * apps.py
+    * forms.py
+    * managers.py
+    * models.py
+    * serializers.py
+    * urls.py
+    * views.py
+* .gitignore
+* manage.py
+* poetry.lock
+* pyproject.toml
+* readme.md
+
+## Requirements
+
+```
+1. Python>=3.8
+2. Django>=5.x
+3. Postgres
+4. Redis
+```
+
+
+## Basic Setup Instructions
+
+1. **Install Poetry**
+
+   If you don't have Poetry installed, you can install it using the following command:
+
+   ```bash
+   curl -sSL https://install.python-poetry.org | python3 -
+   ```
+   For more installation options, refer to the Poetry documentation.
+
+2. **Clone the respository**
+    Open your terminal or command prompt and navigate to the directory where you want to clone the repository. Then, run the following command to clone the repository:
+    ```bash
+    cd git clone https://github.com/Saurab-Shrestha/django-chat.git
+    cd django-chat
+    ```
+
+3. **Install project dependencies**
+    Since the repository already contains the poetry.lock and pyproject.toml files, you can install the project dependencies by running the following command:
+    ```bash
+    poetry install
+    ```
+    Poetry will install all the required dependencies listed in the `poetry.lock` file.
+    
+4. **Start the Django project**
+    After installing the dependencies, you can start the Django project by running the following commands:
+    ```
+    poetry run python manage.py migrate
+    poetry run python manage.py runserver
+    ```
+    Then, you can view every apis through `http://127.0.0.1:8000/schema/swagger-ui/`
+
